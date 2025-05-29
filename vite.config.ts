a228cdf9be471ffcf6a7871e0b3adefebcfc0774
Daniel <contact@danni.cool>
2025-05-30 00:54:08 +0800

    chore: update Vite configuration for output formats and minification settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,23 @@ export default defineConfig({
     lib: {
       entry: './lib/main.ts',
       name: 'twind-scope',
-      fileName: 'twind-scope',
+      fileName: (format) =>
+        `twind-scope.${
+          format === 'umd' ? 'js' : format === 'es' ? 'esm.js' : 'js'
+        }`,
+      formats: ['es', 'umd'],
     },
+    minify: 'terser',
     terserOptions: {
       compress: {
+        unused: true,
         drop_console: true,
         drop_debugger: true,
       },
+      format: {
+        comments: false,
+      },
+      mangle: true,
     },
   },
 })
